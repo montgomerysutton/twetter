83b21346d0d3408738e1e30698d0dc3773484e85
Montgomery Sutton <montgomery@lupoblu.com>
2015-02-13 17:55:51 -0600

    Update TwetsHelper to make sure the matched handle is a user before replacing the text with a link to a profile page.

--- app/helpers/twets_helper.rb
+++ app/helpers/twets_helper.rb
@@ -1,5 +1,7 @@
 module TwetsHelper
 	def filter_twet( twet_text )
-		return twet_text.gsub(/@(\w+)/) {|username| link_to(username, "/#{$1}").html_safe}.html_safe
+		twet_text.gsub!(/@(\w+)/) { |handle| User.find_by_username("#{$1}") ? link_to(handle, "/#{$1}") : handle }
+
+		return twet_text.html_safe
 	end
 end
