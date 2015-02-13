959912c1ca1e50bea510dc83abad2e79b8203ae3
Montgomery Sutton <montgomery@lupoblu.com>
2015-02-13 17:30:39 -0600

    First try at creating profile pages and linking to users from their '@' handle within all posts.

--- app/helpers/twets_helper.rb
+++ app/helpers/twets_helper.rb
@@ -1,2 +1,5 @@
 module TwetsHelper
+	def filter_twet( twet_text )
+		return twet_text.gsub(/@(\w+)/) {|username| link_to(username, "/#{$1}").html_safe}.html_safe
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Twetter::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get ':username', :to => 'twets#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
